87676bed222897025a942f3037ed6dd32f210483
Sathvik Sethuraman <sathviksethuraman@Jananis-MacBook-Air.local>
2025-07-02 13:54:25 -0700

    finished problem 9 with comments

--- Homework/homework7/9problem.py
+++ Homework/homework7/9problem.py
@@ -1,10 +1,16 @@
+# imports
 from random import randint
 
-list_of_outcome=[]
+# initialize variables
+list_of_outcomes=[]
 
+# generate random outcomes 10K times
+# and append to list
 for i in range(10000):
-    m=randint(1,6)+randint(1,6)
-    list_of_outcomes+=[m]
+    random_outcome=randint(1,6)+randint(1,6)
+    list_of_outcomes+=[random_outcome]
 
-for x in range()
-list_of_outcomes.count(x)
+# calcuate statistics and print
+for possible_outcome in range(2,13):
+    ct=list_of_outcomes.count(possible_outcome)
+    print(possible_outcome,ct/10000*100)
