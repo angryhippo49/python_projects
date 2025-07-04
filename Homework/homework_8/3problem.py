de4b50d0fe0ea5208d9c1f4f12e86b0faa877b7e
Sathvik Sethuraman <sathviksethuraman@Jananis-MacBook-Air.local>
2025-07-03 22:21:29 -0700

    Word shuffler Homework 8 Problem 3 For loop to While loop

--- Homework/homework_8/3problem.py
+++ Homework/homework_8/3problem.py
@@ -7,15 +7,14 @@ words = text.split()
 
 # Create a new list to store randomized elements
 new_list = []
-# random index loop
-new_list = []
-for i in range(len(words)):
 
+# random index loop
+while len(words) > 0:
     # create a temporary variable to store the random word
-    temp=words[randint(0,len(words)-1)]
+    temp = words[randint(0,len(words)-1)]
 
     # add that random word to the new list
-    new_list+=[temp]
+    new_list += [temp]
 
     # remove the random word from the origional list to prevent repetition
     words.remove(temp)
@@ -27,7 +26,7 @@ new_string = ""
 for i in range (len(new_list)):
 
     # add each word of the shuffled list to the empty string plus a space
-    new_string+= new_list[i]+" "
+    new_string += new_list[i]+" "
 
 # print out the shuffled sentence
 print(new_string)
