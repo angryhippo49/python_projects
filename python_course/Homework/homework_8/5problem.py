d67018557d52b72572a7152a82eba6533a14d97a
Sathvik Sethuraman <sathviksethuraman@Jananis-MacBook-Air.local>
2025-07-07 13:21:55 -0700

    random quote problem 5 homework 8

---
+++ python_course/Homework/homework_8/5problem.py
@@ -0,0 +1,7 @@
+# imports
+from random import randint
+
+# Create a list to host the different quotes
+quotes = ["Don't be so humble, you're not that great","Have you ever noticed that anybody driving slower than you is an idiot, and anyone going faster than you is a maniac?","Why do they call it rush hour when nothing moves?","I'm not gay...just desperate","I'm not gay...just desperate","When in doubt, look intelligent","A man who says his wife can't take a joke, forgets that she took him."]
+
+print(quotes[randint(0,6)])
