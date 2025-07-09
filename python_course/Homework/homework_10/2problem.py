181713d3b73156600d5fa134b02d9fd067071c25
Sathvik <sathvikkavisethuraman@gmail.com>
2025-07-08 22:07:44 -0700

    Update 2problem.py

--- python_course/Homework/homework_10/2problem.py
+++ python_course/Homework/homework_10/2problem.py
@@ -8,21 +8,12 @@ def add_excitement(L: List) -> None:
         L[i] = new_L[i]+'!'
         
     
-def change_variable(l: int) -> None:
-    print("l inside function, before change: ", l) # 9
-    l = l + 1
-    print("l inside function, after change: ", l) # 10
-    return
+
 
 if __name__ == '__main__':
     # ask user for a list   
-    # my_list=eval(input("Enter a list: "))
+    my_list=eval(input("Enter a list: "))
 
     # call add_excitement
-    # add_excitement(my_list)
-    # print(my_list)
-
-    l = 9
-    print("l before function call: ", l) # 9
-    change_variable(l)
-    print("l after function call: ", l) # 10
+    add_excitement(my_list)
+    print(my_list)
