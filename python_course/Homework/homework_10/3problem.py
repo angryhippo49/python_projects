d35e77af64d43d2c64a9832d0b99ed278806e04f
Sathvik Sethuraman <sathviksethuraman@Jananis-MacBook-Air.local>
2025-07-08 22:49:15 -0700

    problem 3 homework 10

---
+++ python_course/Homework/homework_10/3problem.py
@@ -0,0 +1,15 @@
+# create a function to add the digits of an integer
+def sum_digits(x:int)->int:
+    # create a variable to add the digits to
+    sum=0
+    for i in range(x):
+        digit=x%10
+        sum+=digit
+        x=int(x/10)
+    return sum
+
+
+if __name__ == '__main__':
+    num=eval(input("Enter a number: "))
+    num=sum_digits(num)
+    print("The digits added up are ",num)
