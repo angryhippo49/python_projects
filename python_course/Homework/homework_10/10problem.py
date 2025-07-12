90c74b605cc44934eabf1f0cd42b0d4152681ae3
Sathvik Sethuraman <sathviksethuraman@Jananis-MacBook-Air.local>
2025-07-12 14:12:20 -0700

    problem 10 homework 10

---
+++ python_course/Homework/homework_10/10problem.py
@@ -0,0 +1,23 @@
+# imports
+from typing import List
+
+# function for closest element
+def closest_to_n(L: List, n: int) -> int:
+    mtlist = []
+    closest = -9999
+    for x in L:
+        if x <= n:
+            mtlist += [x]
+    for i in mtlist:
+        if i > closest:
+            closest = i
+    return closest
+
+
+
+# main function
+if __name__ == "__main__":
+    L = eval(input("Enter a list: "))
+    n = eval(input("Enter the number you want to find the closest to: "))
+    answer = closest_to_n(L, n)
+    print(answer)
